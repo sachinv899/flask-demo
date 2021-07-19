a205b1c41ae97caa317d0d4335c9ac07510f71aa
sachinv899 <sachinv899@gmail.com>
2021-07-19 16:47:56 +0530

    heroku git:remote -a falkapp

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 import json
 
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('view.html')
 
 @app.route("/getStudent")
 def hello_world():
