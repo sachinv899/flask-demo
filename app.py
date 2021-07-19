736566262601850587b7d74e95816106adad8213
sachinv899 <sachinv899@gmail.com>
2021-07-19 17:50:04 +0530

    hehe

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ def hello():
     return render_template('view.html')
     
 
-# @app.route("/getStudent",methods=['POST','GET'])
-# def hello_world():
-#     x = {
-#   "name": "John",
-#   "age": 30,
-#   "city": "New York"
-# }
-#     return json.dumps(x)
+@app.route("/getStudent",methods=['POST','GET'])
+def hello_world():
+    x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+    return json.dumps(x)
 
 if __name__ == "__main__":
     app.run(debug=True)
